28d5213d58e527f0fe9e2d88d48d962f53f8019d
Sinan KULEN <sinan.kulen@gmail.com>
2021-05-16 19:17:39 +0300

    Initial Commit

---
+++ TrainerProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrainerProject
+//
+//  Created by Sinan Kulen on 16.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
